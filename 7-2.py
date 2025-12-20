d5769297f37d5ae53bb2a1e6cd3dbd95086e25db
Coder109 <3211697682@qq.com>
2025-12-20 15:08:24 +0800

    Update 7-2

--- 7-2.py
+++ 7-2.py
@@ -5,22 +5,35 @@ graph = []
 for line in content:
     graph.append([])
     for c in line.strip():
-        graph[-1].append(c if c != "S" else "1")
+        if c == "S":
+            graph[-1].append([1])
+        elif c == ".":
+            graph[-1].append([0])
+        elif c == "^":
+            graph[-1].append([-1])
+        else:
+            raise NotImplementedError
+
+barrier = -1
+for line_idx in range(1, len(graph[1:])+1):
+    for elem_idx in range(len(graph[line_idx])):
+        upper_flow = graph[line_idx-1][elem_idx][0]
+        curr_terrain = graph[line_idx][elem_idx][0]
+        # Flow to barrier
+        if curr_terrain == barrier:
+            if elem_idx == 0:
+                graph[line_idx][elem_idx+1][0] += upper_flow
+            elif elem_idx == len(graph[line_idx]) - 1:
+                graph[line_idx][elem_idx-1][0] += upper_flow
+            else:
+                graph[line_idx][elem_idx-1][0] += upper_flow
+                graph[line_idx][elem_idx+1][0] += upper_flow
+        # Flow naturally
+        elif curr_terrain != barrier and upper_flow != barrier:
+            graph[line_idx][elem_idx][0] += upper_flow
 
 result = 0
-
-for line_idx in range(len(graph[1:])):
-    for char_idx in range(len(graph[line_idx])):
-        if graph[line_idx-1][char_idx] == "1" and graph[line_idx][char_idx] == "^":
-            result += 1
-            if char_idx == 0 :
-                graph[line_idx][char_idx+1] = "1"
-            elif char_idx == len(graph[line_idx]) - 1:
-                graph[line_idx][char_idx-1] = "1"
-            else:
-                graph[line_idx][char_idx-1] = "1"
-                graph[line_idx][char_idx+1] = "1"
-        elif graph[line_idx-1][char_idx] == "1":
-            graph[line_idx][char_idx] = "1"
-
+for lst in graph[-1]:
+    curr_result = lst[0]
+    result += curr_result if curr_result > barrier else 0
 print(result)
